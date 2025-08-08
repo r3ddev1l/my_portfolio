ea800246b7f2db86f0fe2618d4ea974cc1cb688f
SrijanNeoSoftware <srijanmaharjan1@gmail.com>
2025-08-08 21:00:37 +0545

    Feature: Dark mode toggle added[1;6D[1;6D[C[C.

---
+++ assets/lib/l10n/app_localizations_en.dart
@@ -0,0 +1,19 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get comingSoon => 'Coming Soon!';
+
+  @override
+  String get underConstruction => 'This site is under construction.';
+
+  @override
+  String get srijanMaharjanPortfolio => 'Srijan Maharjan\'s Portfolio';
+}
